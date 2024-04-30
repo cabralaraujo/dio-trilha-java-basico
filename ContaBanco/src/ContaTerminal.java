bb6673cbd48ea50ef454e74e952a876bf3f4da9b
Joilton <joilto@outlook.com>
2024-04-30 05:34:42 -0400

    Excercio criar conta concluido

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int agencia;
+        int numero;
+        String nome;
+        double saldo;
+
+//Entrada de Dados
+        System.out.println("Por favor digite seu nome !");
+        nome = sc.next();
+
+        System.out.println("Por favor, digite o Numero da Agência !");
+        agencia = sc.nextInt();
+
+        System.out.println("Por digite o numero da conta !");
+        numero = sc.nextInt();
+
+        System.out.println("Digite o valor do Deposíto");
+        saldo = sc.nextDouble();
+
+//Saida de Dados
+        System.out.println("Ola " + nome + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
