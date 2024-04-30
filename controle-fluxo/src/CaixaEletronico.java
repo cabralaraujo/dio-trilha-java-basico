3e75b73be39383f29e5e80f33d613b06fecfd17d
Joilton <joilto@outlook.com>
2024-04-30 07:15:05 -0400

    Fim da aula estruturas condicionais

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -7,9 +7,11 @@ public class CaixaEletronico {
         if (valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
               System.out.println("Novo saldo: " + saldo);
-    }else
-            System.out.println("Saldo insufuciente !");
 
+        else
+            System.out.println("Saldo insufuciente !");
+        
 
     }
+}
 }
